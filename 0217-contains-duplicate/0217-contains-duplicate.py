3f123eec04e9166d0451795ef34df73f3868d822
Rafae Rajani <rafaerajani@gmail.com>
2026-09-21 15:18:26 +1000

    Update 0217-contains-duplicate.py

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -6,5 +6,13 @@ class Solution(object):
                 return True
             seen.add(num)
         return False
-
+        
+    # Notes
+    #     - the reason we didnt compare the length of nums to the set nums is because
+    #       on average it will be slower since it takes O(n) to convert a list to a set
+    #     - this solution has better average performance, and the same worst case
+    #       performance
+    
+    # Time Complexity = O(n)
+    # Space Complexity = O(n)
         

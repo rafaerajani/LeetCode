cf3a71826ef12b4d9dd52224a5dd8a4a980bfd9c
Rafae Rajani <rafaerajani@gmail.com>
2026-09-21 14:09:24 +1000

    Enhance getConcatenation with comments
    Added comments explaining the enumeration and complexity.

--- 1929-concatenation-of-array/1929-concatenation-of-array.py
+++ 1929-concatenation-of-array/1929-concatenation-of-array.py
@@ -1,7 +1,10 @@
 class Solution(object):
     def getConcatenation(self, nums):
         ans = [0]*(2*len(nums))
+        # enumerate so that you can get both the number and the index in one go
         for i, num in enumerate(nums):
             ans[i] = num
             ans[len(nums) + i] = num
         return ans
+
+        # for n numbers in num, the time complexity of this is O(n) and space complexity is O(2n) which simplifies to O(n)
